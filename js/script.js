43f3df7150461ef67e240b8b8578d946ca71911d
Josue Ramirez <josueramirez0110@gmail.com>
2022-07-21 23:53:30 -0500

    Navbar, section one and section two completed

---
+++ js/script.js
@@ -0,0 +1,9 @@
+// Script to activate the hamburger menu
+
+const hamburger = document.querySelector(".hamburger");
+const navMenu = document.querySelector(".nav-menu");
+
+hamburger.addEventListener("click", () => {
+  hamburger.classList.toggle("active");
+  navMenu.classList.toggle("active");
+})
