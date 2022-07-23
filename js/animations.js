7ee73188c3871c1c0885672a3a03a7beefbf3207
Josue Ramirez <josueramirez0110@gmail.com>
2022-07-23 04:11:07 -0500

    Section three and four completed

---
+++ js/animations.js
@@ -0,0 +1,27 @@
+window.sr = ScrollReveal();
+
+sr.reveal('.img-iphone', {
+  duration: 3000,
+});
+
+  sr.reveal('.photo1', {
+    duration: 4000,
+    origin: 'top',
+    distance: '150px'
+  });
+
+  sr.reveal('.photo2', {
+    duration: 4000,
+  });
+
+  sr.reveal('.photo3', {
+    duration: 4000,
+    origin: 'right',
+    distance: '400px'
+  });
+
+  sr.reveal('.camera-iphone', {
+    duration: 4000,
+    origin: 'bottom',
+    distance: '400px'
+  });
